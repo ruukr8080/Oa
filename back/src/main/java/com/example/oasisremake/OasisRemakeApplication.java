7c0b817fc906e8315344224b4edd9baeac9eb9a4
han <131014787+ruukr8080@users.noreply.github.com>
2024-11-22 04:03:01 +0900

    init

---
+++ back/src/main/java/com/example/oasisremake/OasisRemakeApplication.java
@@ -0,0 +1,13 @@
+package com.example.oasisremake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OasisRemakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OasisRemakeApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/example/oasisremake/OasisRemakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oasisremake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OasisRemakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
